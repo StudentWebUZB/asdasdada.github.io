846b1be36206b32d9301b91fc9c330ccb59cfc62
Ilhomov Diyorbek <Diyorbekk>
2022-08-04 18:40:46 +0500

    js arguments

--- js/test.js
+++ js/test.js
@@ -53,18 +53,109 @@
 // console.log(malumotlarim.ishlari[4].davlat)
 
 
-let a = 10
-a *= 3
-// let result = a+b
-console.log(a)
+// let a = 10
+// a *= 3
+// // let result = a+b
+// console.log(a)
+//
+// const news_list = ["Jahon", "Milliy", "Sport", "Xujjum"]
+//
+//
+//
+// for (var i = 0; i < news_list.length; i++) {
+//     document.querySelector("#news").innerHTML += `<p>${i === 3 ?  "Xujjimchi" : news_list[i]}</p>`
+//    // ещё какие-то выражения
+// }
+//
 
-const news_list = ["Jahon", "Milliy", "Sport", "Xujjum"]
 
-
-
-for (var i = 0; i < news_list.length; i++) {
-    document.querySelector("#news").innerHTML += `<p>${i === 3 ?  "Xujjimchi" : news_list[i]}</p>`
-   // ещё какие-то выражения
+var object = {
+    name: "Tashkent",
+    year: 1999,
+    ussd: "sum"
 }
 
+console.log(object)
+
+object.name = "Samarqand"
+
+
+object["people"] = 3000000
+
+
+// delete object["people"]
+
+
+console.log(object)
+
+
+let array = [30000, 20, 400000, 5000, 20, "adasdasdsa", true]
+// array.push(8000000)
+// array[0] = 200000
+
+// console.log(array.shift()) // boshini olib qoladi qoganlarini ob tashidi
+// console.log(array.pop()) // oxirgisini olib qoladi qoganlarini ob tashidi
+// console.log(array)
+
+// console.log(array.indexOf(20, -1))
+const f = array.entries();
+
+array.map((item, key) => {
+    // console.log(key + "\n" + item)
+    // document.querySelector("#news").innerHTML += `<p>${item}</p>`
+})
+
+for (let x of f) {
+    // document.getElementById("news").innerHTML += x + "<br>";
+}
+
+
+
+
+
+
+
+// const result = array.filter(item => item === 20);
+// document.querySelector("#search").innerHTML += "<hr>" + result
+// console.log(result)
+// console.log(array.slice(1, 4)) // orqalig'ini kesadi
+//
+// console.log(typeof array[1].toString())
+//
+// console.log(Array.isArray(9))
+//
+//
+// let fruits = ["Olma", "Anor", "Banan", 'Kivi']
+//
+//
+// document.querySelector("#news").innerHTML += `<p>${fruits.join("<br>")}</p>`
+//
+// console.log()
+
+
+// array.map((item, number) => {
+//     console.log(number)
+//     console.log(item)
+//     document.querySelector("#news").innerHTML += `<p>${item}</p>`
+// })
+
+x = 4
+y = 4
+
+a = 5
+b = 5
+
+
+// console.log(x == y) // Shunga yaqin
+// console.log(x === y) // Bu Aniqlik (Xaqiqat)
+// console.log(x != y) // Shunga yaqin 4 = 4 teng bomasa
+// console.log(x !== y) // (Xaqiqat) 4 = 4 teng bomasa
+
+
+// console.log(x >= y) // Katta teng
+// console.log(x <= y) // Kichik teng
+
+
+console.log(x === y && a !== b)
+console.log(x === y || a !== b)
 
